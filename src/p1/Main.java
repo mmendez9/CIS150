617869c4c32f6bad10d0d15f3066f5f03142ca1a
unknown <mmendez9@W-261-09.dtcc.edu>
2016-01-19 09:54:45 -0500

    Initial commit

---
+++ src/p1/Main.java
@@ -0,0 +1,11 @@
+package p1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double [] grades = {84, 88, 90, 80, 99};
+
+        for (double each : grades)
+            System.out.println(each);
+    }
+}
